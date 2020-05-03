9ff5858f2114880a95f57ff528f427f4d6c0f55e
sai <sai@990323.com>
2020-05-03 11:26:09 +0800

    update

--- BufMgr/src/buffer.cpp
+++ BufMgr/src/buffer.cpp
@@ -14,78 +14,213 @@
 #include "exceptions/bad_buffer_exception.h"
 #include "exceptions/hash_not_found_exception.h"
 
-namespace badgerdb { 
+namespace badgerdb
+{
 
 BufMgr::BufMgr(std::uint32_t bufs)
-	: numBufs(bufs) {
+		: numBufs(bufs)
+{
 	bufDescTable = new BufDesc[bufs];
 
-  for (FrameId i = 0; i < bufs; i++) 
-  {
-  	bufDescTable[i].frameNo = i;
-  	bufDescTable[i].valid = false;
-  }
+	for (FrameId i = 0; i < bufs; i++)
+	{
+		bufDescTable[i].frameNo = i;
+		bufDescTable[i].valid = false;
+	}
 
-  bufPool = new Page[bufs];
+	bufPool = new Page[bufs];
 
-	int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
-  hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
+	int htsize = ((((int)(bufs * 1.2)) * 2) / 2) + 1;
+	hashTable = new BufHashTbl(htsize); // allocate the buffer hash table
 
-  clockHand = bufs - 1;
+	clockHand = bufs - 1;
 }
+BufMgr::~BufMgr()
+{
+	// flushes out all dirty pages
+	for (FrameId i = 0; i < numBufs; i++) {
+		if (bufDescTable[i].dirty && bufDescTable[i].valid)
+		{
+			bufDescTable[i].file->writePage(bufPool[i]);
+			bufDescTable[i].Clear();
+		}
+	}
 
+	//deallocates the buffer pool
+	delete[] bufPool;
 
-BufMgr::~BufMgr() {
+	//deallocates the BufDesc table
+	delete[] bufDescTable;
 }
 
 void BufMgr::advanceClock()
 {
-
+	clockHand = (clockHand + 1) % numBufs;
 }
 
-void BufMgr::allocBuf(FrameId & frame) 
+void BufMgr::allocBuf(FrameId &frame)
 {
-}
+	std::int64_t pinned = 0;
+	advanceClock();
 
-	
-void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
-{
-}
-
-
-void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
-{
-}
-
-void BufMgr::flushFile(const File* file) 
-{
-}
-
-void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
-{
-}
-
-void BufMgr::disposePage(File* file, const PageId PageNo)
-{
-    
-}
-
-void BufMgr::printSelf(void) 
-{
-  BufDesc* tmpbuf;
-	int validFrames = 0;
-  
-  for (std::uint32_t i = 0; i < numBufs; i++)
+	while (bufDescTable[clockHand].valid)
 	{
-  	tmpbuf = &(bufDescTable[i]);
+		if (bufDescTable[clockHand].refbit)
+		{
+			bufDescTable[clockHand].refbit = false;
+			advanceClock();
+			continue;
+		}
+
+		if (bufDescTable[clockHand].pinCnt)
+		{
+			pinned++;
+			if (pinned == numBufs)
+			{
+				throw BufferExceededException();
+			}
+			advanceClock();
+			continue;
+		}
+
+		if (bufDescTable[clockHand].dirty)
+		{
+			bufDescTable[clockHand].file->writePage(bufPool[clockHand]);
+			bufDescTable[clockHand].dirty = false;
+		}
+
+		hashTable->remove(bufDescTable[clockHand].file, bufDescTable[clockHand].pageNo);
+		break;
+	}
+
+	bufDescTable[clockHand].Clear();
+	frame = clockHand;
+}
+
+void BufMgr::readPage(File *file, const PageId pageNo, Page *&page)
+{
+	FrameId frameId;
+	try
+	{
+		hashTable->lookup(file, pageNo, frameId);
+		bufDescTable[frameId].refbit = true;
+		bufDescTable[frameId].pinCnt++;
+	}
+	catch (HashNotFoundException e)
+	{
+		allocBuf(frameId);
+
+		Page newPage = file->readPage(pageNo);
+		hashTable->insert(file, pageNo, frameId);
+		bufPool[frameId] = newPage;
+		bufDescTable[frameId].Set(file, pageNo);
+	}
+
+	page = &bufPool[frameId];
+}
+
+void BufMgr::unPinPage(File *file, const PageId pageNo, const bool dirty)
+{
+	try
+	{
+		FrameId frameId;
+		hashTable->lookup(file, pageNo, frameId);
+
+		if (bufDescTable[frameId].pinCnt == 0)
+		{
+			throw PageNotPinnedException(file->filename(), pageNo, frameId);
+		}
+		
+		bufDescTable[frameId].pinCnt--;
+
+		if (dirty)
+		{
+			bufDescTable[frameId].dirty = true;
+		}
+	}
+	catch (HashNotFoundException e)
+	{
+	}
+}
+
+void BufMgr::flushFile(const File *file)
+{
+	for (FrameId i = 0; i < numBufs; i++)
+	{
+		if (bufDescTable[i].file == file)
+		{
+
+			if (bufDescTable[i].pinCnt)
+			{
+				throw PagePinnedException(file->filename(), bufDescTable[i].pageNo, i);
+			}
+
+			if (!bufDescTable[i].valid)
+			{
+				throw BadBufferException(i, bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
+			}
+
+			if (bufDescTable[i].dirty)
+			{
+				bufDescTable[i].file->writePage(bufPool[i]);
+				bufDescTable[i].dirty = false;
+			}
+
+			hashTable->remove(file, bufDescTable[i].pageNo);
+
+			bufDescTable[i].Clear();
+		}
+	}
+}
+
+void BufMgr::allocPage(File *file, PageId &pageNo, Page *&page)
+{
+	Page newPage = file->allocatePage();
+
+	FrameId frameId;
+	allocBuf(frameId);
+
+	pageNo = newPage.page_number();
+	hashTable->insert(file, pageNo, frameId);
+	bufPool[frameId] = newPage;
+	bufDescTable[frameId].Set(file, pageNo);
+
+	page = &bufPool[frameId];
+}
+
+void BufMgr::disposePage(File *file, const PageId PageNo)
+{
+	try
+	{
+		FrameId frameId;
+
+		hashTable->lookup(file, PageNo, frameId);
+		hashTable->remove(file, PageNo);
+		bufDescTable[frameId].Clear();
+	}
+	catch (HashNotFoundException e)
+	{
+	}
+
+	file->deletePage(PageNo);
+}
+
+void BufMgr::printSelf(void)
+{
+	BufDesc *tmpbuf;
+	int validFrames = 0;
+
+	for (std::uint32_t i = 0; i < numBufs; i++)
+	{
+		tmpbuf = &(bufDescTable[i]);
 		std::cout << "FrameNo:" << i << " ";
 		tmpbuf->Print();
 
-  	if (tmpbuf->valid == true)
-    	validFrames++;
-  }
+		if (tmpbuf->valid == true)
+			validFrames++;
+	}
 
 	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
 }
 
-}
+} // namespace badgerdb
--- BufMgr/src/main.cpp
+++ BufMgr/src/main.cpp
@@ -13,12 +13,12 @@
 #include "exceptions/page_pinned_exception.h"
 #include "exceptions/buffer_exceeded_exception.h"
 
-#define PRINT_ERROR(str) \
-{ \
-	std::cerr << "On Line No:" << __LINE__ << "\n"; \
-	std::cerr << str << "\n"; \
-	exit(1); \
-}
+#define PRINT_ERROR(str)                            \
+	{                                                 \
+		std::cerr << "On Line No:" << __LINE__ << "\n"; \
+		std::cerr << str << "\n";                       \
+		exit(1);                                        \
+	}
 
 using namespace badgerdb;
 
@@ -27,7 +27,7 @@ PageId pid[num], pageno1, pageno2, pageno3, i;
 RecordId rid[num], rid2, rid3;
 Page *page, *page2, *page3;
 char tmpbuf[100];
-BufMgr* bufMgr;
+BufMgr *bufMgr;
 File *file1ptr, *file2ptr, *file3ptr, *file4ptr, *file5ptr;
 
 void test1();
@@ -38,64 +38,70 @@ void test5();
 void test6();
 void testBufMgr();
 
-int main() 
+int main()
 {
 	//Following code shows how to you File and Page classes
 
-  const std::string& filename = "test.db";
-  // Clean up from any previous runs that crashed.
-  try
+	const std::string &filename = "test.db";
+	// Clean up from any previous runs that crashed.
+	try
 	{
-    File::remove(filename);
-  }
-	catch(FileNotFoundException)
+		File::remove(filename);
+	}
+	catch (FileNotFoundException &)
 	{
-  }
+	}
 
-  {
-    // Create a new database file.
-    File new_file = File::create(filename);
-    
-    // Allocate some pages and put data on them.
-    PageId third_page_number;
-    for (int i = 0; i < 5; ++i) {
-      Page new_page = new_file.allocatePage();
-      if (i == 3) {
-        // Keep track of the identifier for the third page so we can read it
-        // later.
-        third_page_number = new_page.page_number();
-      }
-      new_page.insertRecord("hello!");
-      // Write the page back to the file (with the new data).
-      new_file.writePage(new_page);
-    }
+	{
+		// Create a new database file.
+		File new_file = File::create(filename);
 
-    // Iterate through all pages in the file.
-    for (FileIterator iter = new_file.begin();
-         iter != new_file.end();
-         ++iter) {
-      // Iterate through all records on the page.
-      for (PageIterator page_iter = (*iter).begin();
-           page_iter != (*iter).end();
-           ++page_iter) {
-        std::cout << "Found record: " << *page_iter
-            << " on page " << (*iter).page_number() << "\n";
-      }
-    }
+		// Allocate some pages and put data on them.
+		PageId third_page_number;
+		for (int i = 0; i < 5; ++i)
+		{
+			Page new_page = new_file.allocatePage();
+			if (i == 2)
+			{
+				// Keep track of the identifier for the third page so we can read it
+				// later.
+				third_page_number = new_page.page_number();
+			}
+			new_page.insertRecord("hello!");
+			// Write the page back to the file (with the new data).
+			new_file.writePage(new_page);
+		}
 
-    // Retrieve the third page and add another record to it.
-    Page third_page = new_file.readPage(third_page_number);
-    const RecordId& rid = third_page.insertRecord("world!");
-    new_file.writePage(third_page);
+		// Iterate through all pages in the file.
+		for (FileIterator iter = new_file.begin();
+				 iter != new_file.end();
+				 ++iter)
+		{
+			auto page = *iter;
+			// Iterate through all records on the page.
+			std :: cout << page.begin() << " " << page.end()  << std::endl;
+			for (PageIterator page_iter = page.begin();
+					 page_iter != page.end();
+					 ++page_iter)
+			{
+				std::cout << "Found record: " << *page_iter
+									<< " on page " << page.page_number() << "\n";
+			}
+		}
 
-    // Retrieve the record we just added to the third page.
-    std::cout << "Third page has a new record: "
-        << third_page.getRecord(rid) << "\n\n";
-  }
-  // new_file goes out of scope here, so file is automatically closed.
+		// Retrieve the third page and add another record to it.
+		Page third_page = new_file.readPage(third_page_number);
+		const RecordId &rid = third_page.insertRecord("world!");
+		new_file.writePage(third_page);
 
-  // Delete the file since we're done with it.
-  File::remove(filename);
+		// Retrieve the record we just added to the third page.
+		std::cout << "Third page has a new record: "
+							<< third_page.getRecord(rid) << "\n\n";
+	}
+	// new_file goes out of scope here, so file is automatically closed.
+
+	// Delete the file since we're done with it.
+	File::remove(filename);
 
 	//This function tests buffer manager, comment this line if you don't wish to test buffer manager
 	testBufMgr();
@@ -107,23 +113,23 @@ void testBufMgr()
 	bufMgr = new BufMgr(num);
 
 	// create dummy files
-  const std::string& filename1 = "test.1";
-  const std::string& filename2 = "test.2";
-  const std::string& filename3 = "test.3";
-  const std::string& filename4 = "test.4";
-  const std::string& filename5 = "test.5";
+	const std::string &filename1 = "test.1";
+	const std::string &filename2 = "test.2";
+	const std::string &filename3 = "test.3";
+	const std::string &filename4 = "test.4";
+	const std::string &filename5 = "test.5";
 
-  try
+	try
 	{
-    File::remove(filename1);
-    File::remove(filename2);
-    File::remove(filename3);
-    File::remove(filename4);
-    File::remove(filename5);
-  }
-	catch(FileNotFoundException e)
+		File::remove(filename1);
+		File::remove(filename2);
+		File::remove(filename3);
+		File::remove(filename4);
+		File::remove(filename5);
+	}
+	catch (FileNotFoundException e)
 	{
-  }
+	}
 
 	File file1 = File::create(filename1);
 	File file2 = File::create(filename2);
@@ -138,15 +144,15 @@ void testBufMgr()
 	file5ptr = &file5;
 
 	//Test buffer manager
-	//Comment tests which you do not wish to run now. Tests are dependent on their preceding tests. So, they have to be run in the following order. 
+	//Comment tests which you do not wish to run now. Tests are dependent on their preceding tests. So, they have to be run in the following order.
 	//Commenting  a particular test requires commenting all tests that follow it else those tests would fail.
 	test1();
 	test2();
 	test3();
 	test4();
 	test5();
-	test6();
 
+	delete bufMgr;
 	//Close files before deleting them
 	file1.~File();
 	file2.~File();
@@ -155,15 +161,20 @@ void testBufMgr()
 	file5.~File();
 
 	//Delete files
-	File::remove(filename1);
-	File::remove(filename2);
-	File::remove(filename3);
-	File::remove(filename4);
-	File::remove(filename5);
+	
+	{
+		File::remove(filename1);
+		File::remove(filename2);
+		File::remove(filename3);
+		File::remove(filename4);
+		File::remove(filename5);
+	}
 
-	delete bufMgr;
 
-	std::cout << "\n" << "Passed all tests." << "\n";
+
+	std::cout << "\n"
+						<< "Passed all tests."
+						<< "\n";
 }
 
 void test1()
@@ -172,7 +183,7 @@ void test1()
 	for (i = 0; i < num; i++)
 	{
 		bufMgr->allocPage(file1ptr, pid[i], page);
-		sprintf((char*)tmpbuf, "test.1 Page %d %7.1f", pid[i], (float)pid[i]);
+		sprintf((char *)tmpbuf, "test.1 Page %d %7.1f", pid[i], (float)pid[i]);
 		rid[i] = page->insertRecord(tmpbuf);
 		bufMgr->unPinPage(file1ptr, pid[i], true);
 	}
@@ -181,14 +192,15 @@ void test1()
 	for (i = 0; i < num; i++)
 	{
 		bufMgr->readPage(file1ptr, pid[i], page);
-		sprintf((char*)&tmpbuf, "test.1 Page %d %7.1f", pid[i], (float)pid[i]);
-		if(strncmp(page->getRecord(rid[i]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
+		sprintf((char *)&tmpbuf, "test.1 Page %d %7.1f", pid[i], (float)pid[i]);
+		if (strncmp(page->getRecord(rid[i]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
 		{
 			PRINT_ERROR("ERROR :: CONTENTS DID NOT MATCH");
 		}
 		bufMgr->unPinPage(file1ptr, pid[i], false);
 	}
-	std::cout<< "Test 1 passed" << "\n";
+	std::cout << "Test 1 passed"
+						<< "\n";
 }
 
 void test2()
@@ -196,35 +208,35 @@ void test2()
 	//Writing and reading back multiple files
 	//The page number and the value should match
 
-	for (i = 0; i < num/3; i++) 
+	for (i = 0; i < num / 3; i++)
 	{
 		bufMgr->allocPage(file2ptr, pageno2, page2);
-		sprintf((char*)tmpbuf, "test.2 Page %d %7.1f", pageno2, (float)pageno2);
+		sprintf((char *)tmpbuf, "test.2 Page %d %7.1f", pageno2, (float)pageno2);
 		rid2 = page2->insertRecord(tmpbuf);
 
 		int index = random() % num;
-    pageno1 = pid[index];
+		pageno1 = pid[index];
 		bufMgr->readPage(file1ptr, pageno1, page);
-		sprintf((char*)tmpbuf, "test.1 Page %d %7.1f", pageno1, (float)pageno1);
-		if(strncmp(page->getRecord(rid[index]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
+		sprintf((char *)tmpbuf, "test.1 Page %d %7.1f", pageno1, (float)pageno1);
+		if (strncmp(page->getRecord(rid[index]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
 		{
 			PRINT_ERROR("ERROR :: CONTENTS DID NOT MATCH");
 		}
 
 		bufMgr->allocPage(file3ptr, pageno3, page3);
-		sprintf((char*)tmpbuf, "test.3 Page %d %7.1f", pageno3, (float)pageno3);
+		sprintf((char *)tmpbuf, "test.3 Page %d %7.1f", pageno3, (float)pageno3);
 		rid3 = page3->insertRecord(tmpbuf);
 
 		bufMgr->readPage(file2ptr, pageno2, page2);
-		sprintf((char*)&tmpbuf, "test.2 Page %d %7.1f", pageno2, (float)pageno2);
-		if(strncmp(page2->getRecord(rid2).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
+		sprintf((char *)&tmpbuf, "test.2 Page %d %7.1f", pageno2, (float)pageno2);
+		if (strncmp(page2->getRecord(rid2).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
 		{
 			PRINT_ERROR("ERROR :: CONTENTS DID NOT MATCH");
 		}
 
 		bufMgr->readPage(file3ptr, pageno3, page3);
-		sprintf((char*)&tmpbuf, "test.3 Page %d %7.1f", pageno3, (float)pageno3);
-		if(strncmp(page3->getRecord(rid3).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
+		sprintf((char *)&tmpbuf, "test.3 Page %d %7.1f", pageno3, (float)pageno3);
+		if (strncmp(page3->getRecord(rid3).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
 		{
 			PRINT_ERROR("ERROR :: CONTENTS DID NOT MATCH");
 		}
@@ -232,14 +244,16 @@ void test2()
 		bufMgr->unPinPage(file1ptr, pageno1, false);
 	}
 
-	for (i = 0; i < num/3; i++) {
-		bufMgr->unPinPage(file2ptr, i+1, true);
-		bufMgr->unPinPage(file2ptr, i+1, true);
-		bufMgr->unPinPage(file3ptr, i+1, true);
-		bufMgr->unPinPage(file3ptr, i+1, true);
+	for (i = 0; i < num / 3; i++)
+	{
+		bufMgr->unPinPage(file2ptr, i + 1, true);
+		bufMgr->unPinPage(file2ptr, i + 1, true);
+		bufMgr->unPinPage(file3ptr, i + 1, true);
+		bufMgr->unPinPage(file3ptr, i + 1, true);
 	}
 
-	std::cout << "Test 2 passed" << "\n";
+	std::cout << "Test 2 passed"
+						<< "\n";
 }
 
 void test3()
@@ -249,11 +263,12 @@ void test3()
 		bufMgr->readPage(file4ptr, 1, page);
 		PRINT_ERROR("ERROR :: File4 should not exist. Exception should have been thrown before execution reaches this point.");
 	}
-	catch(InvalidPageException e)
+	catch (InvalidPageException e)
 	{
 	}
 
-	std::cout << "Test 3 passed" << "\n";
+	std::cout << "Test 3 passed"
+						<< "\n";
 }
 
 void test4()
@@ -265,18 +280,20 @@ void test4()
 		bufMgr->unPinPage(file4ptr, i, false);
 		PRINT_ERROR("ERROR :: Page is already unpinned. Exception should have been thrown before execution reaches this point.");
 	}
-	catch(PageNotPinnedException e)
+	catch (PageNotPinnedException e)
 	{
 	}
 
-	std::cout << "Test 4 passed" << "\n";
+	std::cout << "Test 4 passed"
+						<< "\n";
 }
 
 void test5()
 {
-	for (i = 0; i < num; i++) {
+	for (i = 0; i < num; i++)
+	{
 		bufMgr->allocPage(file5ptr, pid[i], page);
-		sprintf((char*)tmpbuf, "test.5 Page %d %7.1f", pid[i], (float)pid[i]);
+		sprintf((char *)tmpbuf, "test.5 Page %d %7.1f", pid[i], (float)pid[i]);
 		rid[i] = page->insertRecord(tmpbuf);
 	}
 
@@ -286,11 +303,12 @@ void test5()
 		bufMgr->allocPage(file5ptr, tmp, page);
 		PRINT_ERROR("ERROR :: No more frames left for allocation. Exception should have been thrown before execution reaches this point.");
 	}
-	catch(BufferExceededException e)
+	catch (BufferExceededException e)
 	{
 	}
 
-	std::cout << "Test 5 passed" << "\n";
+	std::cout << "Test 5 passed"
+						<< "\n";
 
 	for (i = 1; i <= num; i++)
 		bufMgr->unPinPage(file5ptr, i, true);
@@ -299,7 +317,8 @@ void test5()
 void test6()
 {
 	//flushing file with pages still pinned. Should generate an error
-	for (i = 1; i <= num; i++) {
+	for (i = 1; i <= num; i++)
+	{
 		bufMgr->readPage(file1ptr, i, page);
 	}
 
@@ -308,13 +327,14 @@ void test6()
 		bufMgr->flushFile(file1ptr);
 		PRINT_ERROR("ERROR :: Pages pinned for file being flushed. Exception should have been thrown before execution reaches this point.");
 	}
-	catch(PagePinnedException e)
+	catch (PagePinnedException e)
 	{
 	}
 
-	std::cout << "Test 6 passed" << "\n";
+	std::cout << "Test 6 passed"
+						<< "\n";
 
-	for (i = 1; i <= num; i++) 
+	for (i = 1; i <= num; i++)
 		bufMgr->unPinPage(file1ptr, i, true);
 
 	bufMgr->flushFile(file1ptr);
